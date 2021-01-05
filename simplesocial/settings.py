59025bef87cb789b20c4a7801ba9085fbe6cc39e
Andrew Lorbieski <andrew.lorbieski@gmail.com>
2021-01-04 20:56:29 -0600

    Update STATIC_ROOT.

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_ROOT = os.path.join('staticfiles')
 
 LOGIN_REDIRECT_URL = 'test'
 LOGOUT_REDIRECT_URL = 'thanks'
