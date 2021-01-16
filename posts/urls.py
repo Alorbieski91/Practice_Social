8436581c41d11d318232c65674075b0597d3a84e
Andrew Lorbieski <andrew.lorbieski@gmail.com>
2021-01-16 12:53:19 -0600

    Remove unnecessary re_path code in urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, re_path
+from django.urls import path
 from . import views
 
 app_name = 'posts'
@@ -6,7 +6,7 @@ app_name = 'posts'
 urlpatterns = [
     path('', views.PostList.as_view(), name='all'),
     path('new/', views.CreatePost.as_view(), name='create'),
-    re_path(r'by/(?P<username>[-\w]+)', views.UserPosts.as_view(), name='for_user'),
-    re_path(r'by/(?P<username>[-\w]+)/(?P<pk>\d+)/$', views.PostDetail.as_view(), name='single'),
-    re_path(r'delete/(?P<pk>\d+)/$', views.DeletePost.as_view(), name='delete'),
+    path('by/<username>', views.UserPosts.as_view(), name='for_user'),
+    path('by/<username>/<pk>/', views.PostDetail.as_view(), name='single'),
+    path('delete/<pk>/', views.DeletePost.as_view(), name='delete'),
 ]
