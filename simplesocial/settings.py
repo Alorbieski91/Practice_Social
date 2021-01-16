93cd98e8b4746b50467b31abae9259209e22d592
Andrew Lorbieski <andrew.lorbieski@gmail.com>
2021-01-16 00:23:40 -0600

    Add bootstrap icon template tags.

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django_bootstrap_icons',
     'bootstrap4',
     'accounts',
     'groups',
